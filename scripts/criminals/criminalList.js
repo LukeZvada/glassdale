eb5bef4a8e010c2fe7105ccf109dd1b77193c1b5
LukeZvada <zvadamusic@gmail.com>
2020-07-27 14:57:18 -0500

    wrapped the .find and .filter in a if/else statement so when please select a crime is selected it shows all of the criminals.

--- scripts/criminals/criminalList.js
+++ scripts/criminals/criminalList.js
@@ -6,26 +6,32 @@ const contentTarget = document.querySelector(".criminalsContainer")
 const eventHub = document.querySelector(".container")
 
 eventHub.addEventListener("crimeSelected", (crimeSelectedEvent) => {
-    // the goal here is to Filter displayed criminals by the crime that was chosen by the user in the dropdown menu.
+    // the goal here is to Filter displayed criminals by the crime that was chosen by the user in the dropdown menu. If they select "please select a crime.. show all of the criminals."
 
     // Looking for what crime was chosen
     const crimeThatWasSelected = crimeSelectedEvent.detail.crimeId  // 9
+    //console.log(crimeThatWasSelected) used this console.log to figure out what the id was of "please select a crime.." 
+    
+    if (crimeThatWasSelected === "0") { //Used the crimeid to figure out if the "please select a crime.." was selected. 
+        const allCriminals = useCriminals() // invoked the  useCriminals() function to get all of the criminal and render it
+        render(allCriminals) 
+    } else { // if crimeId was anything other than 0. Find the crime that was selected using .find then filter convictions out.
+        // Get actual crime name. Number is not enough.
+        const arrayOfCrimes = useConvictions()
+        const foundCrimeObject = arrayOfCrimes.find(
+            (crime) => {
+                return parseInt(crimeThatWasSelected) === crime.id
+            }) // { id: 9, name: "Theft" }
 
-    // Get actual crime name. Number is not enough.
-    const arrayOfCrimes = useConvictions()
-    const foundCrimeObject = arrayOfCrimes.find(
-        (crime) => {
-            return parseInt(crimeThatWasSelected) === crime.id
-        }) // { id: 9, name: "Theft" }
+        // Filter criminal array to only criminal that have a matching `conviction` property value
+        const allCriminals = useCriminals()
 
-    // Filter criminal array to only criminal that have a matching `conviction` property value
-    const allCriminals = useCriminals()
-
-    const filteredCriminals = allCriminals.filter(
-        (currentCriminalObject) => {
-            return foundCrimeObject.name === currentCriminalObject.conviction
+        const filteredCriminals = allCriminals.filter(
+            (currentCriminalObject) => {
+                return foundCrimeObject.name === currentCriminalObject.conviction
         })
-    render(filteredCriminals)
+        render(filteredCriminals)
+    }
 })
 
 const render = (arrayOfCriminals) => {
