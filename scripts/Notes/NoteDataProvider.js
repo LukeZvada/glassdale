35204ec2caa27d0ffb38737564b03ba548220246
LukeZvada <zvadamusic@gmail.com>
2020-07-28 12:03:34 -0500

    added more elements to Notes

---
+++ scripts/Notes/NoteDataProvider.js
@@ -0,0 +1,53 @@
+const eventHub = document.querySelector(".container")
+
+const dispatchStateChangeEvent = () => {
+    const noteStateChangedEvent = new CustomEvent("noteStateChanged")
+
+    eventHub.dispatchEvent(noteStateChangedEvent)
+}
+
+const getNotes = () => {
+    fetch('http://localhost:8088/notes')
+        .then(response => response.json())
+        .then(parsedNotes => {
+            notes = parsedNotes
+        })
+
+}
+
+export const saveNote = note => {
+    fetch('http://localhost:8088/notes', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(note)
+    })
+    .then(getNotes)
+    .then(dispatchStateChangeEvent)
+}
+
+
+
+
+
+
+
+
+
+eventHub.addEventListener("click", clickEvent => {
+    if (clickEvent.target.id === "saveNote") {
+
+        // Make a new object representation of a note
+        const newNote = {
+            // Key/value pairs here
+        }
+
+        // Change API state and application state
+        saveNote(newNote)
+    }
+})
+
+const NoteForm = () => {
+    // rest of the code here
+}
