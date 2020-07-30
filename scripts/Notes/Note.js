a31258d3fbd4b18a6852b6c92fec350648ede181
LukeZvada <zvadamusic@gmail.com>
2020-07-30 11:04:03 -0500

    formatted HTML

--- scripts/Notes/Note.js
+++ scripts/Notes/Note.js
@@ -3,7 +3,7 @@ export const noteHTMLConverter = (noteObject) => {
         <section class="note">
             <divclass="note__Title>Title: ${noteObject.tite}</div> 
             <divclass="note__author>Author: ${noteObject.author}</div> 
-            <divclass="note__author>Content: ${noteObject.content}</div> 
+            <divclass="note__content>Content: ${noteObject.content}</div> 
             <divclass="note__timestamp>Date: ${new Date(noteObject.timestamp).toLocaleDateString( `en-US` )}</div> 
         </section>
     `
