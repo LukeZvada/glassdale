51886b1fdd11fbc675e5de4093aca22ac1b698ee
LukeZvada <zvadamusic@gmail.com>
2020-08-03 13:31:03 -0500

    alibi button updates.

--- scripts/criminals/associateButton.js
+++ scripts/criminals/associateButton.js
@@ -1,10 +1,39 @@
 import { useCriminals, getCriminals } from "./criminalDataProvider.js";
 
+const contentTarget = document.querySelector(".associate__button")
 const eventHub = document.querySelector(".container")
 
-eventHub.addEventListener("click", clickEvent => { 
-    getCriminals().then(() => {
-
-        const criminals = useCriminals()
+contentTarget.addEventListener("change", (changeEvent) => {
+    const customEvent = new CustomEvent("alibiSelected", {
+        detail: {
+            known__associateId: changeEvent.target.value
+        }
     })
+    eventHub.dispatchEvent(customEvent)
 })
+
+export const AssociateSelect = () => { 
+    // *   ConvictionSelect component that renders a select HTML element which lists all convictions in the Glassdale PD API
+    getCriminals().then(() => {
+        // Get all convictions from application state / aplication state means its current condition 
+        const criminals = useCriminals()
+        
+        render(criminals)
+    })
+
+        
+    const render = alibiCollection => {
+
+        contentTarget.innerHTML = `
+            <select class="button" id="crimeSelect">
+                ${
+                    alibiCollection.map(
+                        alibiObject => {
+                            return `<option value="${ alibiObject.id }">${alibiObject.name}</option>`
+                        }
+                    ).join("") //gets rid of the comma in the array when logged
+                }
+            </select>
+        `
+    }
+}
