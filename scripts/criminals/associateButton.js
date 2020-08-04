3e6c5c0690163d32d14ee645928c31d0446bc978
LukeZvada <zvadamusic@gmail.com>
2020-08-04 10:05:36 -0500

    added dialog click event

--- scripts/criminals/associateButton.js
+++ scripts/criminals/associateButton.js
@@ -1,1 +1,17 @@
 import { useCriminals, getCriminals } from "./criminalDataProvider.js";
+
+
+
+
+
+
+
+
+
+export const AssociatesClick = () => {
+    return `
+        <dialog class="associatesClick">
+        
+        </dialog>
+    `
+}
