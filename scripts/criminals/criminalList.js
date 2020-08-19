12abf68a1bccc9d2751cadd464eaf3312991d7b1
LukeZvada <zvadamusic@gmail.com>
2020-08-19 08:56:21 -0700

    added criminalFacilityProvider.js and added code to criminalList.js

--- scripts/criminals/criminalList.js
+++ scripts/criminals/criminalList.js
@@ -89,3 +89,21 @@ export const criminalList = () => {
             render(criminals)
         })
 }
+
+
+export const CriminalList = () => {
+    // Kick off the fetching of both collections of data
+    getFacilities()
+        .then(getCriminalFacilities)
+        .then(
+            () => {
+                // Pull in the data now that it has been fetched
+                const facilities = useFacilities()
+                const crimFac = useCriminalFacilities()
+                const criminals = useCriminals()
+
+                // Pass all three collections of data to render()
+                render(criminals, facilities, crimFac)
+            }
+        )
+}
---
+++ scripts/facility/CriminalFacilityProvider.js
@@ -0,0 +1,13 @@
+let criminalFacilities = []
+
+export const useCriminalFacilities = () => {
+    return criminalFacilities.slice()
+}
+
+export const getCriminalFacilities = () => {
+    return fetch("http://criminals.glassdale.us/criminalFacilities")
+        .then(response => response.json())
+        .then(apiData => {
+            criminalFacilities = apiData
+        })
+}
